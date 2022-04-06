8271e66b8f32cf75a64f999c6937f1fc6b0ae325
Feller,Ruby R <ruby.feller@student.fontys.nl>
2022-04-06 13:28:15 +0200

    Initial commit: added SimpleAlgorithm assignment

--- SimpleAlgorithm/Program.cs
+++ SimpleAlgorithm/Program.cs
@@ -5,12 +5,14 @@ Order order = new Order();
 double maxPriceResult = order.GiveMaximumPrice();
 double avgPriceResult = order.GiveAveragePrice();
 List<Product> result = order.GetAllProducts();
+
 Console.WriteLine("Products:");
 foreach (var item in result)
 {
     Console.WriteLine(item.Name);
     Console.WriteLine(item.Price);
 }
+
 List<Product> sortedList = order.SortProductsByPrice();
 Console.WriteLine("Sorted Products (lowest to highest):");
 foreach (var item in sortedList)
