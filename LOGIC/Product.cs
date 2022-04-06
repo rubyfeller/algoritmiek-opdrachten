558b6831c3fca47a61615a9f7a3083e72525fdea
Feller,Ruby R <ruby.feller@student.fontys.nl>
2022-04-06 13:12:47 +0200

    Add project files.

---
+++ LOGIC/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAlgorithm
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public Double Price { get; set; }
+        public List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product { Name = "Lifejacket", Price = 48.95 });
+            products.Add(new Product { Name = "Soccer ball", Price = 19.50 });
+            products.Add(new Product { Name = "Corner flag", Price = 34.95 });
+
+            return products;
+        }
+    }
+}
