792b03bd523394a9721100b49d833c3bbdf1145d
Feller,Ruby R <ruby.feller@student.fontys.nl>
2022-04-23 15:09:14 +0200

    Initial commit of 'CircusTrein'

---
+++ CircusTrein/CircusTrein.Logic/Enums/Size.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircusTrein.Logic.Enums
+{
+    public class Size
+    {
+        public enum SizeEnum
+        {
+            Small,
+            Medium,
+            Large
+        }
+        public SizeEnum GenerateSize()
+        {
+            Random rnd = new Random();
+            int size = rnd.Next(0, 3);
+            return (SizeEnum)size;
+        }
+    }
+}
---
+++ CircusTrein/CircusTrein.Logic/Models/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircusTrein.Logic.Models
+{
+    // Base class
+    public abstract class Animal
+    {
+        public Animal(int size)
+        {
+            Size = size;
+        }
+
+        public int Size { get; set; }
+    }
+}
