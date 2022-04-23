931c5b18e72ea34022a83ce10588b70b5d5cd7a7
Feller,Ruby R <ruby.feller@student.fontys.nl>
2022-04-23 15:38:05 +0200

    .

---
+++ CircusTrein/CircusTrein.Logic/Models/Carnivore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircusTrein.Logic.Models
+{
+    // Eats meat
+    public class Carnivore : Animal
+    {
+        public Carnivore(int size) : base(size)
+        {
+        }
+    }
+}
---
+++ CircusTrein/CircusTrein/Program.cs
@@ -0,0 +1,17 @@
+﻿using CircusTrein.Logic.Enums;
+using CircusTrein.Logic.Models;
+
+Console.WriteLine("Hello Circus Train");
+
+// Generate carnivores
+Size size = new Size();
+var carnivoreSize = size.GenerateSize();
+Carnivore carnivore = new Carnivore((int)carnivoreSize);
+carnivore.Size = (int)carnivoreSize;
+Console.WriteLine("Size carnivore: " + carnivoreSize);
+
+// Generate herbivores
+var herbivoreSize = size.GenerateSize();
+Herbivore herbivore = new Herbivore((int)herbivoreSize);
+herbivore.Size = (int)herbivoreSize;
+Console.WriteLine("Size herbivore: " + herbivoreSize);
